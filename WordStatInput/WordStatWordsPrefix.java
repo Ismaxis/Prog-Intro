7ce3301905c97f2e2cde14b8ea4769bcd80f4fb0
Ismaxis <max200412345@gmail.com>
2022-10-02 02:53:54 +0300

    mod

--- WordStatInput/WordStatWordsPrefix.java
+++ WordStatInput/WordStatWordsPrefix.java
@@ -1,29 +1,32 @@
 import java.io.*;
-import java.util.LinkedHashMap;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeSet;
-import java.util.Map.Entry;
 
 
 public class WordStatWordsPrefix {
-    static final int BUFFER_SIZE = 1024;
+    private static final int BUFFER_SIZE = 1024;
     public static void main(String[] args) {
         try {
-            LinkedHashMap<String, Integer> hashMap = countWordsInFile(args[0]);
-
-            outputResult(hashMap, args[1]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Not enougth command line variables (2 expected): " + e.getMessage());
+            if (args.length >= 2) {
+                Map<String, Integer> map = countWordsInFile(args[0]);
+                outputResult(map, args[1]);
+            } else {
+                System.out.println("Not enougth command line args (2 expected)");
+            }
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
+        } catch (UnsupportedEncodingException e) {   
+            System.out.println("Unsupported encoding: " + e.getMessage());
         } catch (IOException e) {   
             System.out.println("Input/Output error: " + e.getMessage());
+            e.printStackTrace(System.out);
         }
     }
 
-    public static LinkedHashMap<String, Integer> countWordsInFile(String fileName) throws FileNotFoundException, IOException {
-        LinkedHashMap<String, Integer> hashMap = new LinkedHashMap<>();
-        BufferedReader reader = createReader(fileName);
+    public static HashMap<String, Integer> countWordsInFile(String fileName) throws FileNotFoundException, IOException {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF8"), BUFFER_SIZE); 
         try {
             char[] buffer = new char[BUFFER_SIZE];
             int read = reader.read(buffer);
@@ -39,10 +42,10 @@ public class WordStatWordsPrefix {
         return hashMap;
     }
 
-    public static void outputResult(LinkedHashMap<String, Integer> hashMap, String fileName) throws FileNotFoundException, IOException {
-        BufferedWriter writer = createWriter(fileName); 
+    public static void outputResult(Map<String, Integer> hashMap, String fileName) throws FileNotFoundException, IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), "UTF8"));  
         try {
-            TreeSet<String> sortedSet = new TreeSet(hashMap.keySet());
+            TreeSet<String> sortedSet = new TreeSet<>(hashMap.keySet());
             for (String prefics : sortedSet) {
                 writer.write(prefics + " " + hashMap.get(prefics));
                 writer.newLine();   
@@ -52,8 +55,7 @@ public class WordStatWordsPrefix {
         }
     }
 
-
-    public static String parseWords(LinkedHashMap<String, Integer> hashMap, String source) {
+    public static String parseWords(Map<String, Integer> hashMap, String source) {
         String reminder = "";
         for (int i = 0; i < source.length(); i++) {
             int start = i;
@@ -65,29 +67,31 @@ public class WordStatWordsPrefix {
                     break;
                 }
             }
+
             if (start < i && reminder.isEmpty()) {
-                String key = source.substring(start, start + 3).toLowerCase();
-                int value = hashMap.containsKey(key) ? hashMap.get(key) : 0;
+                String key = source.substring(start, min(start + 3, i)).toLowerCase();
+                int value = hashMap.getOrDefault(key, 0);
                 hashMap.put(key, value + 1);
             }
         }
-
         return reminder;
     }
 
     public static boolean isPartOfWord(char ch) {
         return Character.isLetter(ch) || Character.DASH_PUNCTUATION == Character.getType(ch) || ch == '\'';
     }
-
+    
     public static BufferedReader createReader(String fileName) throws FileNotFoundException, UnsupportedEncodingException {
-        return new BufferedReader(
-            new InputStreamReader(
-                new FileInputStream(fileName), "UTF8"), BUFFER_SIZE); 
+        return new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF8"), BUFFER_SIZE); 
     }
-
+    
     public static BufferedWriter createWriter(String fileName) throws FileNotFoundException, UnsupportedEncodingException {
         return new BufferedWriter(
                 new OutputStreamWriter(
-                    new FileOutputStream(fileName), "UTF8"));
+                    new FileOutputStream(fileName), "UTF8"));         
+    }
+
+    public static int min(int a, int b) {
+        return a <= b ? a : b;
     }
 }
