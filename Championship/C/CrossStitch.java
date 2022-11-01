17b9908cbf6e5b18e9dd392538d636b316b12873
Ismaxis <Ismaxis@github.com>
2022-11-01 10:01:49 +0300

    C-14test

--- Championship/C/CrossStitch.java
+++ Championship/C/CrossStitch.java
@@ -1,78 +1,64 @@
 package C;
+import java.util.Scanner;
 import java.util.Stack;
 
-import myscanner.*;
 
 public class CrossStitch {
     public static void main(String[] args) {
-        // MyScanner scn = new MyScanner(System.in, new WhiteSpace());
-        // String str = """
-        //             9 6
-        //             XX.XXX.XX
-        //             .X.X.X.X.
-        //             .X.X.X.X.
-        //             .X.X.X.X.
-        //             .XXX.X.X.
-        //             .....XXX.
-        //             """;
-        String str = """
-                    7 7
-                    X..XXXX
-                    X..X...
-                    X..X...
-                    XXXXXXX
-                    ...X..X
-                    ...X..X
-                    XXXX..X
-                    """;
-        MyScanner scn = new MyScanner(str, new WhiteSpace());
-
-        int x = scn.nextInt();
-        int y = scn.nextInt();
-
-        int vertCount = (x + 1) * (y + 1);
-
-        int[][] faceEdges = new int[vertCount][vertCount];
-        int[][] backEdges = new int[vertCount][vertCount]; 
-
-        int[] startVertex = {0,0};
-        scn.nextLine();
-        for (int i = 0; i < y; i++) {
-            String line = scn.nextLine();
-            for (int j = 0; j < x; j++) {
-                if (line.charAt(j) == 'X') {
-                    setEdge(faceEdges, (x+1)*i + j, (x+1)*(i+1) + j + 1, 1); // \
-                    setEdge(faceEdges, (x+1)*(i+1) + j, (x+1)*i + j + 1, 1); // /
-
-                    setEdge(backEdges, (x+1)*i + j, (x+1)*i + j + 1, 1); // _
-                    setEdge(backEdges, (x+1)*i + j, (x+1)*(i+1) + j, 1); // |
-                    setEdge(backEdges, (x+1)*(i+1) + j + 1, (x+1)*i + j + 1, 1); // _
-                    setEdge(backEdges, (x+1)*(i+1) + j + 1, (x+1)*(i+1) + j, 1); // |
-
-                    startVertex[0] = j;
-                    startVertex[1] = i;
+        Scanner scn = new Scanner(System.in);
+        
+        try {
+            int x = scn.nextInt();
+            int y = scn.nextInt();
+    
+            int vertCount = (x + 1) * (y + 1);
+    
+            int[][] faceEdges = new int[vertCount][vertCount];
+            int[][] backEdges = new int[vertCount][vertCount]; 
+    
+            int[] startVertex = {0,0};
+            scn.nextLine();
+            for (int i = 0; i < y; i++) {
+                String line = scn.nextLine();
+                for (int j = 0; j < x; j++) {
+                    if (line.charAt(j) == 'X') {
+                        setEdge(faceEdges, (x+1)*i + j, (x+1)*(i+1) + j + 1, 1); // \
+                        setEdge(faceEdges, (x+1)*(i+1) + j, (x+1)*i + j + 1, 1); // /
+    
+                        setEdge(backEdges, (x+1)*i + j, (x+1)*i + j + 1, 1); // _
+                        setEdge(backEdges, (x+1)*(i+1) + j + 1, (x+1)*(i+1) + j, 1); // _
+                        setEdge(backEdges, (x+1)*i + j, (x+1)*(i+1) + j, 1); // |
+                        setEdge(backEdges, (x+1)*(i+1) + j + 1, (x+1)*i + j + 1, 1); // |
+    
+                        startVertex[0] = j+1;
+                        startVertex[1] = i+1;
+                    }
                 }
             }
-        }
-
-        Stack<int[]> s = new Stack<>();
-        s.push(startVertex);
-
-        boolean isFace = true;
-        while (!s.empty()) {
-            int[] curVertex = s.peek();
-            boolean edgeFound = findEdge(faceEdges, backEdges, isFace, curVertex, s, x ,y);
-
-            if (!edgeFound) {
-                break;
+    
+            Stack<int[]> s = new Stack<>();
+            s.push(startVertex);
+    
+            boolean isFace = true;
+            while (!s.empty()) {
+                int[] curVertex = s.peek();
+                boolean edgeFound = findEdge(faceEdges, backEdges, isFace, curVertex, s, x ,y);
+    
+                if (!edgeFound) {
+                    break;
+                }
+    
+                isFace = !isFace;
             }
 
-            isFace = !isFace;
-        }
+            System.out.println(s.size() - 1);
+            for (int[] is : s) {
+                System.out.println(String.format("%d %d", is[0], is[1]));
+            }
+            
 
-        System.out.println(s.size());
-        for (int[] is : s) {
-            System.out.println(String.format("%d %d", is[0], is[1]));
+        } finally {
+            scn.close();
         }
     }    
 
@@ -81,7 +67,7 @@ public class CrossStitch {
         int[][] mainEdges = isFace ? faceEdges : backEdges;
         int[][] secEdges = isFace ? backEdges : faceEdges;
 
-        for (int i = -1; i < 2; i++) {
+        for (int i = 1; i >= -1; i--) {
             for (int j = -1; j < 2; j++) {
                 int[] secVert = {j + vertex[0], i + vertex[1]};
                 if ((secVert[1] >= 0 && secVert[1] < y + 1) && (secVert[0] >= 0 && secVert[0] < x + 1)) {
@@ -91,7 +77,7 @@ public class CrossStitch {
                             if (secEdges[secIndex][k] == 1) {
                                 s.push(secVert);
                                 if (isFace) {
-                                    setEdge(mainEdges, curIndex, secIndex, 0);
+                                    setEdge(mainEdges, curIndex, secIndex, -1);
                                 }
                                 return true;
                             }
