2c9457d74d5f64573ce990818c2eb1d7d476ecd8
olesya11104 <olesya11104@mail.ru>
2022-12-20 18:05:50 +0300

    Checked by an instructor

--- java-solutions/expression/exceptions/CheckedMultiply.java
+++ java-solutions/expression/exceptions/CheckedMultiply.java
@@ -11,6 +11,7 @@ public class CheckedMultiply extends Multiply {
     @Override
     protected int calc(int left, int right) {
         int r = left * right;
+		// :NOTE: почему именно 15? Немного не хватает комментариев и константы
         if (((Math.abs(left) | Math.abs(right)) >>> 15 != 0)) {
             if (((right != 0) && (r / right != left)) ||
                     (left == Integer.MIN_VALUE && right == -1)) {
